f939f07d1006a2830b33a1d7d121059bb696265c
Allen Chan <chanall@oregonstate.edu>
2021-01-29 20:01:16 -0800

    updated error handle

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
-year = int(input("Enter Year: "))
+while True:
+	try:
+		year = int(input("Enter Year between 0-9999: "))
+		if 0 <= year <= 9999:
+			break
+		raise ValueError()
+
+	except ValueError:
+		print("Enter number between 0 and 9999")
 
 if year % 4 == 0 and year % 100 != 0:
 	print(year, "is a leap year")
